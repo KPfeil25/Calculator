a61c647c753db721729b89ecf03857e9087fe8ed
Kevin Pfeil <kevpfeil25@gmail.com>
2021-03-11 20:46:21 -0800

    changed test file

--- test_calculator.py
+++ test_calculator.py
@@ -4,8 +4,11 @@ Unit tests for the calculator app
 
 import calc
 
+
 class TestCalculator:
     def test1(self):
         assert 5 == calc.add(1, 4)
+
     def test2(self):
         assert 2 == calc.subtract(4, 2)
+        
